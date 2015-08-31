ca873f9b121d77276bb09a47eb57cccc771ea050
Alex Antonyuk <alex@antonyuk.me>
2015-08-31 21:44:02 +0200

    Initial Commit

---
+++ Mapppzzz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mapppzzz
+//
+//  Created by Alex Antonyuk on 8/31/15.
+//  Copyright (c) 2015 Alex Antonyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mapppzzz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mapppzzz
+//
+//  Created by Alex Antonyuk on 8/31/15.
+//  Copyright (c) 2015 Alex Antonyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
